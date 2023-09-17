ccc40d0455f592f1ba3cfdfaaf1ba7507ca8767f
Bonifasius Tarigan <bonifasius.tambak@gmail.com>
2023-09-17 18:05:39 +0700

    Setup project assets

--- app/src/main/java/com/bonifasiustrg/tokopediacloneapp/MainActivity.kt
+++ app/src/main/java/com/bonifasiustrg/tokopediacloneapp/MainActivity.kt
@@ -3,12 +3,15 @@ package com.bonifasiustrg.tokopediacloneapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import com.bonifasiustrg.tokopediacloneapp.ui.theme.TokopediaCloneAppTheme
 
@@ -22,9 +25,29 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    MarketApp()
                 }
             }
         }
+    }
+}
+
+
+@Composable
+fun MarketApp(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .verticalScroll(rememberScrollState())
+    ) {
+//        your code compose here
+    }
+
+}
+
+@Preview(device = Devices.DEFAULT, showBackground = true)
+@Composable
+fun MarketAppPreview() {
+    TokopediaCloneAppTheme {
+        MarketApp()
     }
 }
